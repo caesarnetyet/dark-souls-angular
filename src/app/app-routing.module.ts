222a7fd3f1c15cb23a44edf444d3f537b5114f8c
Crism00 <107272848+Crism00@users.noreply.github.com>
2023-02-21 20:55:18 -0600

    cambios en verification o codigo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChibiComponent } from './components/chibi/chibi.component';
+import { CodigoComponent } from './components/codigo/codigo.component';
 import { RegisterComponent } from './components/register/register.component';
 import {UserComponent} from "./components/user/user.component";
 import {EmployeeComponent} from "./components/employee/employee.component";
@@ -9,6 +10,9 @@ import {AuthGuard} from "./guards/auth.guard";
 
 
 const routes: Routes = [
+  {
+    path: 'verification', component: CodigoComponent
+  },
   {
     path: 'chibi', component: ChibiComponent,
   },
