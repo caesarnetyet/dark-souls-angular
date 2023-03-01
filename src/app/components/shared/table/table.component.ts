ded7710037e89efc1d4fe67cd3414b495a93db5e
Crism00 <107272848+Crism00@users.noreply.github.com>
2023-02-28 22:25:06 -0600

    Revert "Añadido boton de cerrar formulario en los edits"

--- src/app/components/shared/table/table.component.ts
+++ src/app/components/shared/table/table.component.ts
@@ -16,13 +16,9 @@ export class TableComponent implements OnInit {
   @Output() updateEvent = new EventEmitter<Model<any>>();
   @Output() deleteEvent = new EventEmitter<string>();
   updateRow?: Model<object>;
-  mostrarHijo!: boolean;
 
   constructor(private userService: UserService ) {}
 
-  private onCerrarHijo(cerrado: boolean) {
-    this.mostrarHijo = !cerrado;
-  }
 
   async ngOnInit() {
   }
@@ -35,7 +31,6 @@ export class TableComponent implements OnInit {
   }
 
   update(row: Model<object>) {
-    this.onCerrarHijo(this.mostrarHijo)
     this.updateRow = row
     this.updateEvent.emit(row)
   }
